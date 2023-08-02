2e0a5ec79dca535575cabbd8c901c541ca8521f5
thoak90codes <adorahtho@gmail.com>
2023-08-01 23:40:58 -0500

    added useState to Header.jsx file to set currentPage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,45 @@
+import { useState } from 'react';
 import Navigation from './Navigation';
+import AboutMe from './pages/AboutMe'
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+import Resume from './pages/Resume'
 
+export default function Header() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
 
-function Header() {
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return(
     <div>
       <header>
         <h1>Adorah Tho</h1>
-        <Navigation />
+        <Navigation currentPage={currentPage} handlePageChange={handlePageChange}/>
       </header>
       <main>
         <div className="container">
-        <img src="/assets/images/main-background.png" className="main-background" alt="Orange and blue data analytics background"/>
-        <img src="/assets/images/selfie.jpg" className="selfie-image" alt="Selfie"/>
-        <p className="background-text">Hi, I&apos;m Adorah!</p>
-      </div>
+          <img src="/assets/images/main-background.png" className="main-background" alt="Orange and blue data analytics background"/>
+          <img src="/assets/images/selfie.jpg" className="selfie-image" alt="Selfie"/>
+          <p className="background-text">Hi, I&apos;m Adorah!</p>
+        </div>
+        <section>
+          {renderPage()}
+        </section>
       </main>
     </div>
   )
 }
 
-export default Header;
-
-// header has the navigation inside of it. bring in navigation in here.
