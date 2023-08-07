3417c177f19cf37e8c24e7f509750354b51057bd
thoak90codes <adorahtho@gmail.com>
2023-08-07 00:33:28 -0500

    added contact info and form to Contact.jsx file

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,9 +1,40 @@
 export default function Contact() {
   return (
-    <div>
-      <h1>
-        Contact Page
-      </h1>
+    <div className="form-container">
+      <h3>
+        Contact Information:
+      </h3>
+      <br></br>
+      <p>
+        Email: adorahtho@gmail.com
+      </p>
+      <p>
+        Phone #: 555.555.5555
+      </p>
+      <br></br>
+      <br></br>
+      <form className="contact-form">
+        <h3 id="contact-title">Send me a message:</h3>
+        <br></br>
+        <div>
+          <label>Name:</label>
+          <input />
+        </div>
+        <br></br>
+        <div>
+          <label>Email:</label>
+          <input />
+        </div>
+        <br></br>
+        <div>
+          <label>Message:</label>
+          <textarea className="message-textbox"></textarea>
+        </div>
+        <br></br>
+        <div>
+          <button>Send</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
